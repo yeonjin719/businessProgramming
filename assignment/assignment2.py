9b629a7a53244814dda6d133a5dd75f9e9da200b
김연진 <kyj0719@gmail.com>
2025-03-11 11:41:20 +0900

    feat: 2주차 과제

---
+++ assignment/assignment2.py
@@ -0,0 +1,8 @@
+import math
+print('반지름을 입력해주세요: ' , end='')
+r = float(input())
+pie=3.14
+extend = pie * r**2
+circumference = 2 * pie * r
+print('원의 넓이: ', math.floor(extend * 100)/100)
+print('원의 둘레: ', math.floor(circumference * 100)/100)
