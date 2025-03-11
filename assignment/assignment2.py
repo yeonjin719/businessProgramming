8acc53402b19c47b4bd3b47a1149a4ccebed2f94
김연진 <kyj0719@gmail.com>
2025-03-12 00:48:57 +0900

    feat: 백업

--- assignment/assignment2.py
+++ assignment/assignment2.py
@@ -1,8 +1,13 @@
 import math
-print('반지름을 입력해주세요: ' , end='')
-r = float(input())
-pie=3.14
-extend = pie * r**2
-circumference = 2 * pie * r
-print('원의 넓이: ', math.floor(extend * 100)/100)
-print('원의 둘레: ', math.floor(circumference * 100)/100)
+r = float(input('반지름을 입력해주세요: '))
+pi=3.14
+area = pi * r**2
+circumference = 2 * pi * r
+
+# 반올림의 경우
+print(f'원의 넓이: {round(area, 2)}')
+print(f'원의 둘레: {round(circumference, 2)}')
+
+# 버림의 경우
+# print('원의 넓이: ', math.floor( area * pi * r**2 * 100)/100)
+# print('원의 둘레: ', math.floor(circumference * 100)/100)
