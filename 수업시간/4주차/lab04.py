cd7f19585a7516ecb6f4c6a68a7f0bfa7374f1c8
김연진 <kyj0719@gmail.com>
2025-04-01 12:00:31 +0900

    feat: 수정

--- 수업시간/4주차/lab04.py
+++ 수업시간/4주차/lab04.py
@@ -5,7 +5,7 @@ park = [100, 75, 30, 55]
 ko = [90, 80, 100, 100]
 
 total_score = [hong, kim, lee, park, ko]
-score_info = ['출석', '과제', '중간', '기말']
+score_info = ['출석', '과제', '중간', '기말', '최종점수']
 student = ['홍길동', '김길동', '이길동', '박길동', '고길동']
 
 def calculate(scores):
@@ -13,8 +13,6 @@ def calculate(scores):
 
 for i in range(len(student)): 
   print(student[i],'학생의 점수')
-  totalScore = calculate(total_score[i])
-  for j in range(4):
+  total_score[i].append(calculate(total_score[i])) 
+  for j in range(5):
     print(score_info[j], total_score[i][j])
-  print('최종 점수:', totalScore, end='\n\n')
-    
